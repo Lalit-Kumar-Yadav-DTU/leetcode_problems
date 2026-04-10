33edabbca0f04ef6c67de7f17c7f77e3b5cbaaa7
Lalit Kumar Yadav <lalitkumaryadav14112003@gmail.com>
2026-04-10 17:53:38 +0530

    Time: 0 ms (100.00%) | Memory: 30.7 MB (18.28%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -3,21 +3,20 @@ public:
     int minimumDistance(vector<int>& nums) {
         int ans = 0;
         int n = nums.size();
-        vector<int>v;
-        bool flag = true;
+        unordered_map<int,vector<int>>mp;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    if(nums[i] == nums[j] && nums[j] == nums[k]){
-                        int dist = abs(i-j) + abs(j-k) + abs(k-i);
-                        v.push_back(dist);
-                        flag = false;
-                    }
-                }
+            mp[nums[i]].push_back(i);
+        }
+        int minDist = INT_MAX;;
+        for(auto itr: mp){
+            vector<int>&v = itr.second;
+            if(v.size()<3) continue;
+            for(int i=0; i+2<v.size(); i++){
+                int dist = (v[i+2] - v[i]);  //optimized formula 2 * (v[i+2] - v[i])
+                minDist = min(minDist,dist);
             }
         }
-        if(flag) return -1;
-        sort(v.begin(), v.end());
-        return v[0];
+        if(minDist==INT_MAX) return -1;
+        return minDist*2;
     }
 };
