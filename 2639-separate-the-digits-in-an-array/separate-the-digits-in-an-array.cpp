af92e7a27d1d3f8dc09f76e8888eb62515c5fa78
Lalit Kumar Yadav <lalitkumaryadav14112003@gmail.com>
2026-05-11 15:25:59 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (99.01%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums.size();
         for(int i=n-1; i>=0; i--){
             int val = nums[i];
-            while(val){
+            while(val>0){
                 v.push_back(val%10);
                 val /= 10;
             }
